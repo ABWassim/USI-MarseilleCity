274003f6f911d22ed9521ce2012e3fe6e80ee18a
emie.delepine <emie.delepine@gmail.com>
2021-11-11 17:45:31 +0100

    Début test getvideos dans composant video

--- frontend/src/app/video/video.component.ts
+++ frontend/src/app/video/video.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {MessageService} from '../message.service';
+
+export interface Video {
+  title: string;
+  urlVideo : string;
+  channel : string;
+  urlChannel : string;
+  date : Date;
+  thumbnail : string;
+  description : string;
+  platform : string;
+}
 
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.scss']
 })
-export class VideoComponent implements OnInit {
 
-  constructor() { }
+export class VideoComponent implements OnInit {
+  listeVideos: Video[] = [];
+
+  constructor(private msgservice: MessageService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.msgservice.sendMessage('getVideos', null).subscribe(
+      reponse => {
+        this.listeVideos = reponse.data;
+      }
+    );
   }
 
 }
