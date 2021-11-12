f5f7b8e7ca9a40b418412ca8a14a62a444ced90a
emie.delepine <emie.delepine@gmail.com>
2021-11-12 15:06:40 +0100

    Préparation page lecture de video

--- frontend/src/app/zoomvideo/zoomvideo.component.ts
+++ frontend/src/app/zoomvideo/zoomvideo.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
+import {MessageService} from '../message.service';
+import { VideoComponent } from '../video/video.component';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-zoomvideo',
@@ -8,13 +11,28 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 export class ZoomvideoComponent implements OnInit {
   titleVideo = this.route.snapshot.paramMap.get('title');
-  
-  @Input()
-  zoom_video : any
+  videozoom: any;
+  urlVideoYoutube: string = "";
+  urlVideoVimeo: string = "";
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private msgservice: MessageService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const data = {
+      query: "nature"
+    };
+    this.msgservice.sendMessage( environment.debutUrlVideo + '/getVideo', data).subscribe(
+      reponse => {
+        this.videozoom = reponse.data;
+        if (this.videozoom.platform == "Youtube"){
+          this.urlVideoYoutube = "http://www.youtube.com/embed/" + this.videozoom.idVideo;
+        }
+        else{
+          this.urlVideoVimeo = "https://player.vimeo.com/video/" + this.videozoom.idVideo;
+        }
+        console.log(this.videozoom);
+      }
+    );
   }
 
 }
