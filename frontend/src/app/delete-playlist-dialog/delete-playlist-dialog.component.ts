3385283805fc03020d563c3ad00c1c2da2c81c9a
Wassim Ahmed-Belkacem <wassim.ahmed-belkacem@etu.univ-amu.fr>
2021-11-22 16:04:48 +0100

    Suppression de playlist + correction d'un bug qui permettait de créer une playlist à nom-vide

---
+++ frontend/src/app/delete-playlist-dialog/delete-playlist-dialog.component.ts
@@ -0,0 +1,41 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
+import { environment } from 'src/environments/environment';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-delete-playlist-dialog',
+  templateUrl: './delete-playlist-dialog.component.html',
+  styleUrls: ['./delete-playlist-dialog.component.scss']
+})
+export class DeletePlaylistDialogComponent {
+
+  playlistName = this.data.playlistName;
+
+  constructor(public dialogRef: MatDialogRef<DeletePlaylistDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any,
+              @Inject(MessageService) private messageService: MessageService) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onClick(): void {
+    console.log(this.playlistName);
+
+    const data = {
+      name: this.playlistName,
+    };
+
+    this.messageService.sendMessage( environment.debutUrlPlaylist + '/deletePlaylist', data).subscribe(
+      retour => {
+        if (retour.status === 'ok'){
+          this.dialogRef.close(this.playlistName);
+        }
+        else {
+          this.dialogRef.close('');
+        }
+      }
+    );
+  }
+}
