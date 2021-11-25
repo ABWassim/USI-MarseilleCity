921d5fbf468f394bdee9bc34b28ba96ff81eaf05
Wassim Ahmed-Belkacem <wassim.ahmed-belkacem@etu.univ-amu.fr>
2021-11-25 14:54:41 +0100

    Spinner sur la page de recherche + paginateur + composant home retiré

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -10,23 +10,31 @@ import { Video } from '../video/video.component';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  searchw = this.route.snapshot.paramMap.get('searchword');
+  searchw = '';
   listeVideos: Video[] = [];
   searchword = '';
   url = '';
+  showSpinner = true;
+  showPaginator = false;
+  showVideos = false;
 
   constructor(private msgservice: MessageService, private route: ActivatedRoute, private router: Router) { }
   ngOnInit(): void {
     this.msgservice.sendMessage( environment.debutUrlVideo + '/getTrendings', null).subscribe(
       reponse => {
+        this.showSpinner = false;
         this.listeVideos = reponse.data;
+        this.showPaginator = true;
+        this.showVideos = true;
       }
     );
   }
 
   getVideos(i): void {
-    this.url = '/search/' + this.searchword;
-    this.router.navigateByUrl(this.url);
+    this.showVideos = false;
+    this.showPaginator = false;
+    this.showSpinner = true;
+
     const data = {
       query: this.searchword,
       page: i
@@ -34,6 +42,9 @@ export class SearchComponent implements OnInit {
     this.msgservice.sendMessage( environment.debutUrlVideo + '/getVideos', data).subscribe(
       reponse => {
         this.listeVideos = reponse.data;
+        this.showSpinner = false;
+        this.showVideos = true;
+        this.showPaginator = true;
       }
     );
   }
