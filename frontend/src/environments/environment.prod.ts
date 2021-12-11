c9a9045d71f16d9abbc2c8a4ec903f40fdd43598
Wassim Ahmed-Belkacem <wassim.ahmed-belkacem@etu.univ-amu.fr>
2021-12-11 23:06:10 +0100

    Configuration du Frontend pour le déploiement

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  debutUrlUser: 'http://127.0.0.1:3000',
+  debutUrlVideo: 'http://127.0.0.1:3001',
+  debutUrlPlaylist: 'http://127.0.0.1:3002',
+  debutUrlAdverts: 'http://127.0.0.1:3003'
 };
